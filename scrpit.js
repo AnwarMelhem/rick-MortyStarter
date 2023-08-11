edc30fd661835fa56215461bd715312365dd684c
AnwarMelhem <anwarmelhem2000@gmail.com>
2023-08-11 11:36:22 +0300

    first commit

---
+++ scrpit.js
@@ -0,0 +1,32 @@
+
+async function fetchCharacters() {
+  const characterList = document.getElementById('character-list');
+
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character?status=alive');
+    const data = await response.json();
+
+    const aliveCharacters = data.results.slice(0,50);
+    aliveCharacters.forEach(character => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <img src="${character.image}" alt="${character.name}">
+        <div>
+
+          <h3>${character.name}</h3>
+          <h3> id:${character.id}</h3>
+          <p>Location: ${character.location.name}</p>
+          <p>Species: ${character.species}</p>
+          <p>Gender: ${character.gender}</p>
+          <p>Status:${character.status}</p>
+        </div>
+      `;
+      characterList.appendChild(li);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    characterList.innerHTML = '<p>An error occurred while fetching data.</p>';
+  }
+}
+
+fetchCharacters();
